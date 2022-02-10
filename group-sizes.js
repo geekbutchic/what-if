2c7b3fbbbcdb6a2f7b236454e40cf8dd4a75e30f
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-02-10 09:15:32 -0700

    updates with new files, group-sizes.js what-can-you-do.js

--- group-sizes.js
+++ group-sizes.js
@@ -4,4 +4,6 @@ let num = Number(prompt("Enter a class size: "));
 
 if (num % 3 === 0) {
   console.log(`Your group size is ${num}`);
+} else if (num % 3 === 1) {
+    console.log(``);
 }
--- what-can-you-do.js
+++ what-can-you-do.js
@@ -2,12 +2,13 @@ const prompt = require("prompt-sync")();
 
 let age = Number(prompt('Tell us your age and we can tell you what you can do: '));
 
+//logical operators 
 if (age < 16) {
     console.log('You cannot drive 🙊!');
-} else if (age <= 16 || age <= 17) {
+} else if (age === 16 || age === 17) {
     console.log('You can drive but not vote 🏎');
-} else if (age <= 18 || age <= 24) {
+} else if (age >= 18 && age <= 24) {
     console.log('You can vote 🗳  but not rent a car 🚫 🚘');
-} else if (age >= 25) {
+} else {
     console.log('You can do pretty much anything 🥳');
 }
